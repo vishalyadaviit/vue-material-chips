9dd8d12ef032bc91093469d9f673d9cd90c9a19d
nemeton <vyadav@humancaresystems.com>
2020-07-20 02:12:47 +0530

    package.json updated with version as well as homepage and repository added in json to show on npmjs site

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 var path = require("path");
 const { VueLoaderPlugin } = require('vue-loader');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack')
 
 module.exports = {
     entry: "./src/index.js",
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "index.js",
-        library: 'MaterialChips',
+        library: 'VueMaterialChips',
         libraryTarget: 'umd'
     },
     module: {
@@ -74,16 +76,18 @@ module.exports = {
         vue: 'vue'
     },
     resolve: {
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-        ],
         alias: {
             'vue$': 'vue/dist/vue.esm.js'
         },
-        extensions: ['.js', '.json', '.vue']
+        extensions: ['.js', '.vue']
     },
 
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
         new CleanWebpackPlugin(),
         new VueLoaderPlugin()
     ]
